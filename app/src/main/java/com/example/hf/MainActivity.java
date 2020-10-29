e85c597dce1969a39083346852babe2d4bb5e97d
hamdan kasem <58044223+kase4140@users.noreply.github.com>
2020-10-28 20:36:17 -0400

    phase 1

--- app/src/main/java/com/example/hf/MainActivity.java
+++ app/src/main/java/com/example/hf/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.hf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button menuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menuButton = findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, menuPage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/hf/menuPage.java
+++ app/src/main/java/com/example/hf/menuPage.java
@@ -1,21 +1,34 @@
 package com.example.hf;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
+//import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class menuPage extends WearableActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuPage extends AppCompatActivity {
 
     private TextView mTextView;
+    Button exerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
+        exerButton = findViewById(R.id.workexer);
+        exerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(menuPage.this, pageExercise.class);
+                startActivity(intent2);
+            }
+        });
+//        mTextView = (TextView) findViewById(R.id.text);
+//        // Enables Always-on
+//        setAmbientEnabled();
     }
 }
